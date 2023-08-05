f68ba897a3940e7375e5a8e56913ab3e5ec3588d
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-08-05 17:56:42 +0300

    Add redux provider

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Details from './components/Details';
 import Home from './components/Home';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import store from './redux/store';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Nav className="App" />}>
-            <Route index element={<Home />} />
-            <Route path="details" element={<Details />} />
-            <Route path="*" element={<div>Page not found 404!</div>} />
-          </Route>
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Nav className="App" />}>
+              <Route index element={<Home />} />
+              <Route path="/:id" element={<Details />} />
+              <Route path="*" element={<div>Page not found 404!</div>} />
+            </Route>
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
