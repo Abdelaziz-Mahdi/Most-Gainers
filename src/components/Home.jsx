6801fac151a64ce2b3cfc863df6ab39bf15279ac
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-08-05 16:57:17 +0300

    Create cards to display data and filter

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,33 @@ export default function Home() {
       <div className="d-flex flex-column gap-3">
         <figure className="container mb-2 border rounded p-3 bg-success-subtle">
           <div className="row align-items-center">
-            <div className="col-3">
-              <img src="" alt="" className="img-thumbnail" />
+            <div className="col-12 text-center">
+              <h3>Stock Market Most Gainers API</h3>
+              <form action="" className="text-center">
+                <div className=" d-flex justify-content-center">
+                  <div className="col-6">
+                    <input type="text" className="form-control" placeholder="Enter a Company Name" />
+                  </div>
+                </div>
+              </form>
             </div>
-            <div className="col-9">
-              <h3>Home Data</h3>
-              <p>
-                Home 2 Data
-              </p>
+            <div className="row col-12">
+              <div className="card col-6">
+                <div className="card-body">
+                  <h5 className="card-title">Company Name</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">Company Symbol</h6>
+                  <p className="card-text">Company Description</p>
+                  <a href="google.com" className="card-link">Company Website</a>
+                </div>
+              </div>
+              <div className="card col-6">
+                <div className="card-body">
+                  <h5 className="card-title">Company Name</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">Company Symbol</h6>
+                  <p className="card-text">Company Description</p>
+                  <a href="google.com" className="card-link">Company Website</a>
+                </div>
+              </div>
             </div>
           </div>
         </figure>
